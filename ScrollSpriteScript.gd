8a0bcc970f6f2f67ed1d51decc693fbe1b7ca0c4
CuickCoder <120598559+CuickCoder@users.noreply.github.com>
2025-11-27 17:46:42 -0600

    scrollmask keyframe fix & list swaying addded
    fixed the scrollmask showing the list before the scroll started opening, and added a script to the scroll sprite that makes it sway when there's no other animation playing

--- ScrollSpriteScript.gd
+++ ScrollSpriteScript.gd
@@ -1,11 +1,34 @@
 extends TextureRect
+var sway_speed := 1.0
+var max_angle := 1.0
+var sway_timer := 0.0 
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if !is_any_animation_playing():
+		scrollSway(delta)
+	else: 
+		sway_timer = 0.0 #Gotta reset this! Otherwise scroll snaps to previous pos
+		resetScrollRotation(delta)
 	pass
+	
+func scrollSway(delta):
+	sway_timer += delta * sway_speed
+	rotation_degrees = sin(sway_timer) * max_angle
+		
+func resetScrollRotation(delta): 
+	rotation = lerp_angle(rotation, 0.0, delta * 10.0)
+	
+func is_any_animation_playing() -> bool:
+	#grab all the nodes of type "AnimationPlayer"
+	var allAnimPlayers = get_tree().get_root().find_children(
+		"AnimationPlayer","AnimationPlayer",true,false)
+	#return TRUE if any of the animation players we found are playing anything
+	for n in allAnimPlayers:
+		if n.is_playing():
+			#print(n.name + " playing " + n.current_animation)
+			return true
+	#no animationplayers are playing, return false
+	return false
